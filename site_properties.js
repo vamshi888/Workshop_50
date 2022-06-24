a33335ccd71906cb37cecd0546c875f146768878
vamshi888 <vamshik484@gmail.com>
2022-06-24 17:17:09 +0530

    Retrieved Addressbook data from Json server

--- site_properties.js
+++ site_properties.js
@@ -1,4 +1,6 @@
 const site_properties = {
-    home_page: "addressBookHome.html",
-    add_contacts_page: "addresBookForm.html"
-};
+    use_local_storage: "false",
+      home_page: "addressBookHome.html",
+      add_contacts_page: "addresBookForm.html",
+      server_url: "http://127.0.0.1:3000/ContactDB/"
+    };
